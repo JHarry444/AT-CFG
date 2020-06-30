cca60d3b242d6860809d4a504c64940a6ffba884
Jordan <jordan.harrison@qa.com>
2020-06-30 11:04:27 +0100

    Added cucumber

---
+++ src/test/java/com/qa/demo/cuke/DemoTest.java
@@ -0,0 +1,12 @@
+package com.qa.demo.cuke;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/DemoSite.feature")
+public class DemoTest {
+
+}
